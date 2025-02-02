16d1d084affdd4360a49688ca00567aff922c9a7
ryviance <eion.ling0@gmail.com>
2025-02-02 04:03:55 -0800

    Removed Global.js

--- BlockyAnimal.js
+++ BlockyAnimal.js
@@ -2,9 +2,12 @@
 
 // Global variables for camera controls, leg joints, tail joints, head shake, and mouth animation.
 var gl, program, canvas;
+var gAnimalGlobalRotation = 0;
+var gAnimalGlobalRotationX = 0;
 var gCameraZoom = 12;             // Camera distance
 var gCameraAzimuth = 0;           // Camera azimuth (degrees)
 var gCameraElevation = 0;         // Camera elevation (degrees)
+var naturalColorMode = false;
 
 // Leg joint angles (degrees)
 var gFrontLeftLegJointAngle  = 0;
--- Globals.js
+++
@@ -1,9 +0,0 @@
-// Globals.js
-// Global variables for camera controls, leg joints, tail joints, head shake, and mouth animation.
-var gl, program, canvas;
-var gAnimalGlobalRotation = 0;
-var gAnimalGlobalRotationX = 0;
-var gCameraZoom = 12;             // Camera distance
-var gCameraAzimuth = 0;           // Camera azimuth (degrees)
-var gCameraElevation = 0;         // Camera elevation (degrees)
-var naturalColorMode = false;
